2b27f8b4618c916002ebe82b229212086d83038b
SigmaSolutions <36068275+sigmasolution@users.noreply.github.com>
2018-02-12 11:59:51 +0700

    Update deploy_local_windows.sh

--- deploy_local_windows.sh
+++ deploy_local_windows.sh
@@ -1,2 +1,5 @@
-./deploy/plusfun/deploy/sigma-local/build.sh;
-./deploy/plusfun/deploy/sigma-local/runs.sh
+cd D:\project
+D:
+git clone -b develop git@git.coretechnology.pl:plusfun_registration_service/PlusFunRegistrationService.git plusfun
+cd D:\DemoDev
+vagrant up
