9764dfae48afcd6e960e8499c3c9ddf3115f4848
SigmaSolutions <36068275+sigmasolution@users.noreply.github.com>
2018-02-12 12:00:52 +0700

    Update deploy_local_plusfun.sh

--- deploy_local_plusfun.sh
+++ deploy_local_plusfun.sh
@@ -1,4 +1,5 @@
 #git clone   (remember clone from real machine)
+cd /deploy/sigmaSolutions
 git clone git@git.coretechnology.pl:plusfun_registration_service/PlusFunRegistrationService.git plusfun
 #access vagrant
 vagrant up  #start virtual machine and install docker and docker-compose
